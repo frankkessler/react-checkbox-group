00a5177fb06847b44e2b49424629a2b3fced8ec5
Frank Kessler <frankkessler@gmail.com>
2015-03-20 18:11:33 -0400

    Update props transfer to React 0.13.x standards

--- react-checkbox-group.jsx
+++ react-checkbox-group.jsx
@@ -19,8 +19,8 @@ var CheckboxGroup = module.exports = React.createClass({
   },
 
   render: function() {
-    return this.transferPropsTo(
-      <div onChange={this.props.onChange}>
+    return (
+      <div {...this.props} onChange={this.props.onChange}>
         {this.props.children}
       </div>
     );
